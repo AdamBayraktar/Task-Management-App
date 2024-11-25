682eb3aca71ebc3af7a3f449cebe297945e3dfbf
Adam Bayraktar <adambayraktar@gmail.com>
2024-11-25 23:29:11 +0100

    Update: package json with express, mongodb, react libraries and in the server file connect with react app

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 // import { app } from "./app.js";
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+import run from "./db.js";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename);
 
 export const app = express();
-const port = 3000;
+const port = 5000;
 
-app.use(express.static(path.join("..", "task-manager-frontend/build")));
+app.use(express.static(path.join(__dirname, "..", "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client/build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  run();
 });
