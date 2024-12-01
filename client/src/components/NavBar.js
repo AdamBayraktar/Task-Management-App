6a952c05d019bfa42c02d5e864af9a13c4914133
Adam Bayraktar <adambayraktar@gmail.com>
2024-12-01 12:21:55 +0100

    REFACTOR|STYLE: use Link component in navbar and add hover effects

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,29 @@
 import React from "react";
-import "../styles/main.css";
+import { Link } from "react-router-dom";
+import "styles/components/NavBar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <a href="/">Home</a>
-      <a href="/tasks">Tasks</a>
-      <a href="/profile">Profile</a>
-      <a href="/logout" className="logout">
-        Logout
-      </a>
+      {!localStorage.getItem("token") ? (
+        <>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/Register">Register</Link>
+        </>
+      ) : (
+        <>
+          <Link to="/dashboard">Tasks</Link>
+          <Link to="/profile">Profile</Link>
+          <Link
+            to="/logout"
+            onClick={() => localStorage.removeItem("token")}
+            className="logout"
+          >
+            Logout
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
