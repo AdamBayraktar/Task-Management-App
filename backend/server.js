d2135fa08fa6e22e775aa2cb3b50d517f1b00646
Adam Bayraktar <adambayraktar@gmail.com>
2024-12-15 21:50:01 +0100

    FEAT: Add tasks, users routes and use cors, express.json

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
-// import { app } from "./app.js";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import run from "./db.js";
+import { router as taskRoutes } from "./routes/taskRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename);
 
 export const app = express();
+app.use(cors());
+app.use(express.json());
 const port = 5000;
 
+// Routes
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+
+// Serve static assets if in production
 app.use(express.static(path.join(__dirname, "..", "client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client/build", "index.html"));
@@ -17,5 +28,5 @@ app.get("*", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  run();
+  connectDB();
 });
